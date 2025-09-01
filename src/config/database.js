d341a2f763d6a85dfeffc5e13b3ac61a514ccaeb
Iann Bivar <134622570+N0vs@users.noreply.github.com>
2025-09-01 12:11:29 +0100

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,17 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'oceannotes',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+	/* host: process.env.DB_HOST || "localhost",
+	user: process.env.DB_USER || "root",
+	password: process.env.DB_PASSWORD || "",
+	database: process.env.DB_NAME || "oceannotes", */
+	host: process.env.DB_HOST ,
+	user: process.env.DB_USER ,
+	password: process.env.DB_PASSWORD ,
+	database: process.env.DB_NAME ,
+	waitForConnections: true,
+	connectionLimit: 10,
+	queueLimit: 0,
 });
 
 export default pool;
